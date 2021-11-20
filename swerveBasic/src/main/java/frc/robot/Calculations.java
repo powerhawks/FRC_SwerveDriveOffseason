01440a3cbb63e02c124178716f25f8991cdab8cd
powerhawks <teampowerhawks@gmail.com>
2021-11-20 00:14:10 -0500

    F1P-1 Robot now kinda drives, some errors with rotation direction

--- swerveBasic/src/main/java/frc/robot/Calculations.java
+++ swerveBasic/src/main/java/frc/robot/Calculations.java
@@ -10,8 +10,10 @@ import com.revrobotics.CANSparkMax;
 public class Calculations {
     /** TODO FIX ROTATIONAL VELOCITY ISSUE */
     //constructor lmao
-    private final Encoder m_turningEncoderBottomRight = new Encoder(0, 1);
-    private final Encoder m_turningEncoderTopRight = new Encoder (2,3);
+    private final Encoder m_encoderModule0 = new Encoder(0,1);
+    private final Encoder m_encoderModule1 = new Encoder(2,3);
+    private final Encoder m_encoderModule2 = new Encoder(4,5);
+    private final Encoder m_encoderModule3 = new Encoder(6,7);
     public Calculations () {
     
     }
@@ -20,7 +22,7 @@ public class Calculations {
         SmartDashboard.putNumber("X value", xVal);
         SmartDashboard.putNumber("Y value", yVal);
         SmartDashboard.putNumber("Rotation value", rotation);
-        SmartDashboard.putNumber("Encoder", m_turningEncoderBottomRight.getRaw());
+        SmartDashboard.putNumber("Encoder", m_encoderModule3.getRaw());
         bottomRightMotorCalc(xVal, yVal, rotation, maxVelocity);
         TopRightMotorCalc(xVal, yVal, rotation, maxVelocity);
         
@@ -52,7 +54,7 @@ public class Calculations {
         SmartDashboard.putNumber("Resultant Value", resultantVectors);
         SmartDashboard.putNumber("Resultant Angle", resultantAngle);
         SmartDashboard.putNumber("Motor Output", motorOutput);
-        double encoderRotation = (((m_turningEncoderTopRight.getRaw()/4096.000000)%1)*(2*Math.PI));
+        double encoderRotation = (((m_encoderModule0.getRaw()/4096.000000)%1)*(2*Math.PI));
         SmartDashboard.putNumber("RawValue", encoderRotation);
         if (encoderRotation < 0 ) {
             encoderRotation *= -1;
@@ -104,7 +106,7 @@ public class Calculations {
         //SmartDashboard.putNumber("Resultant Value", resultantVectors);
         //SmartDashboard.putNumber("Resultant Angle", resultantAngle);
         //SmartDashboard.putNumber("Motor Output", motorOutput);
-        double encoderRotation = (((m_turningEncoderBottomRight.getRaw()/4096.000000)%1)*(2*Math.PI));
+        double encoderRotation = (((m_encoderModule3.getRaw()/4096.000000)%1)*(2*Math.PI));
         SmartDashboard.putNumber("RawValue", encoderRotation);
         if (encoderRotation < 0 ) {
             encoderRotation *= -1;
@@ -157,22 +159,22 @@ public class Calculations {
         rotVal = Math.abs(rotVal) > 0.03 ? rotVal : 0;
         // top right wheel 0
         double module0Angle = (3 * Math.PI) / 4; // angle from the center of the robot to the module
-        double m0x = xVal + (rotVal * Math.cos(module0Angle)); // add the x components of the translation and rotation
-        double m0y = yVal + (rotVal * Math.sin(module0Angle)); // add the y components of the translation and rotation
+        double m0x = xVal + (rotVal * Math.cos(module0Angle - (Math.PI / 2))); // add the x components of the translation and rotation
+        double m0y = yVal + (rotVal * Math.sin(module0Angle - (Math.PI / 2))); // add the y components of the translation and rotation
         double m0Speed = Math.sqrt( (Math.pow(m0x, 2)) + (Math.pow(m0y, 2))); // find the magnitude of the hypotenuse between the x and y components
         double m0Angle = Math.atan2(m0y, m0x) + Math.PI; // find the angle that the x and y vals make (IMPORTANT! This range is -pi to pi, could break something by not being 0 to 2pi)
 
         // top left wheel 1
         double module1Angle = (5 * Math.PI) / 4; // angle from the center of the robot to the module
-        double m1x = xVal + (rotVal * Math.cos(module1Angle)); // add the x components of the translation and rotation
-        double m1y = yVal + (rotVal * Math.sin(module1Angle)); // add the y components of the translation and rotation
+        double m1x = xVal + (rotVal * Math.cos(module1Angle - (Math.PI / 2))); // add the x components of the translation and rotation
+        double m1y = yVal + (rotVal * Math.sin(module1Angle - (Math.PI / 2))); // add the y components of the translation and rotation
         double m1Speed = Math.sqrt( (Math.pow(m1x, 2)) + (Math.pow(m1y, 2))); // find the magnitude of the hypotenuse between the x and y components
         double m1Angle = Math.atan2(m1y, m1x) + Math.PI; // find the angle that the x and y vals make (IMPORTANT! This range is -pi to pi, could break something by not being 0 to 2pi)
 
         // bottom left wheel 2
         double module2Angle = (7 * Math.PI) / 4; // angle from the center of the robot to the module
-        double m2x = xVal + (rotVal * Math.cos(module2Angle)); // add the x components of the translation and rotation
-        double m2y = yVal + (rotVal * Math.sin(module2Angle)); // add the y components of the translation and rotation
+        double m2x = xVal + (rotVal * Math.cos(module2Angle - (Math.PI / 2))); // add the x components of the translation and rotation
+        double m2y = yVal + (rotVal * Math.sin(module2Angle - (Math.PI / 2))); // add the y components of the translation and rotation
         double m2Speed = Math.sqrt( (Math.pow(m2x, 2)) + (Math.pow(m2y, 2))); // find the magnitude of the hypotenuse between the x and y components
         double m2Angle = Math.atan2(m2y, m2x) + Math.PI; // find the angle that the x and y vals make (IMPORTANT! This range is -pi to pi, could break something by not being 0 to 2pi)
 
@@ -198,7 +200,10 @@ public class Calculations {
              m3Speed /= maxWheelSpeed;
          }
         SmartDashboard.putNumber("RawValue", m3Angle);
-        setSwerveUnitState(Motors.driveMotor3, Motors.turnMotor3, m_turningEncoderBottomRight, m3Speed, m3Angle, velocityScalerDrive, velocityScalerRotate);
+        setSwerveUnitState(Motors.driveMotor0, Motors.turnMotor0, m_encoderModule0, m0Speed, m0Angle, velocityScalerDrive, velocityScalerRotate);
+        setSwerveUnitState(Motors.driveMotor1, Motors.turnMotor1, m_encoderModule1, m1Speed, m1Angle, velocityScalerDrive, velocityScalerRotate);
+        setSwerveUnitState(Motors.driveMotor2, Motors.turnMotor2, m_encoderModule2, m2Speed, m2Angle, velocityScalerDrive, velocityScalerRotate);
+        setSwerveUnitState(Motors.driveMotor3, Motors.turnMotor3, m_encoderModule3, m3Speed, m3Angle, velocityScalerDrive, velocityScalerRotate);
 
     }
 
