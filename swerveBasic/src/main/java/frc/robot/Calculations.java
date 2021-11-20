9c46207acc0ae54bdedf6023bb1947d7c95c96f0
rex-mcall <rexmcall@gmail.com>
2021-11-19 20:08:54 -0500

    F1P-1 rewriting serve kinematics

--- swerveBasic/src/main/java/frc/robot/Calculations.java
+++ swerveBasic/src/main/java/frc/robot/Calculations.java
@@ -5,6 +5,8 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Encoder;
 import java.math.*;
+
+import com.revrobotics.CANSparkMax;
 public class Calculations {
     /** TODO FIX ROTATIONAL VELOCITY ISSUE */
     //constructor lmao
@@ -58,7 +60,7 @@ public class Calculations {
             encoderRotation = (2 * Math.PI) - encoderRotation;
         }
         SmartDashboard.putNumber("Encoder", encoderRotation);
-        Motors.driveMotorTopRight.set(-motorOutput);
+        Motors.driveMotor0.set(-motorOutput);
         
         double rawDistanceFromSetpoint = encoderRotation - resultantAngle;
         double limitedDistance = rawDistanceFromSetpoint % (2 * Math.PI);
@@ -70,10 +72,10 @@ public class Calculations {
             rotationDirection = limitedDistance > 0 ? false : true;
         }
         if (Math.abs(limitedDistance) > (Math.PI / 32)){
-            Motors.turnMotorTopRight.set(rotationDirection ? -0.05 : 0.05);
+            Motors.turnMotor0.set(rotationDirection ? -0.05 : 0.05);
         }
         else {
-            Motors.turnMotorTopRight.set(0);
+            Motors.turnMotor0.set(0);
         }
 
     } 
@@ -110,7 +112,7 @@ public class Calculations {
             encoderRotation = (2 * Math.PI) - encoderRotation;
         }
         //SmartDashboard.putNumber("Encoder", encoderRotation);
-        Motors.driveMotorBottomRight.set(-motorOutput);
+        Motors.driveMotor3.set(-motorOutput);
         
         double rawDistanceFromSetpoint = encoderRotation - resultantAngle;
         double limitedDistance = rawDistanceFromSetpoint % (2 * Math.PI);
@@ -122,13 +124,108 @@ public class Calculations {
             rotationDirection = limitedDistance > 0 ? false : true;
         }
         if (Math.abs(limitedDistance) > (Math.PI / 32)){
-            Motors.turnMotorBottomRight.set(rotationDirection ? -0.05 : 0.05);
+            Motors.turnMotor3.set(rotationDirection ? -0.05 : 0.05);
         }
         else {
-            Motors.turnMotorBottomRight.set(0);
+            Motors.turnMotor3.set(0);
         }
 
-    } 
+    }
+
+    /**
+     * Takes in three joystick values and drives all four swerve units <br><br>
+     * Motors are numbered 0 through 3, starting in the top right (first quadrant) <br><br>
+     * This math works by adding up the component x and y vals of the translation and rotation, then puts the vectors
+     * back together to get the final wheel speed and direction <br><br>
+     * @param xVal
+     * <ul><li> Translation X value from -1 to 1 </ul></li>
+     * @param yVal
+     * <ul><li> Translation Y value from -1 to 1 </ul></li>
+     * @param rotVal
+     * <ul><li> Rotation Value from -1 to 1 </ul></li>
+     * @param velocityScalerDrive
+     * <ul><li> Decimal percentage to run the drive motor (usually 0 to 1 but can be more or less) </ul></li>
+     * @param velocityScalerRotate
+     * <ul><li> Decimal percentage to run the rotation motor (usually 0 to 1 but can be more or less) </ul></li>
+     */
+    public void allWheelDrive(double xVal, double yVal, double rotVal, double velocityScalerDrive, double velocityScalerRotate) {
+        /**
+         * Determines the speed and direction for each motor
+         */
+
+        // top right wheel 0
+        double module0Angle = (3 * Math.PI) / 4; // angle from the center of the robot to the module
+        double m0x = xVal + (rotVal * Math.cos(module0Angle)); // add the x components of the translation and rotation
+        double m0y = yVal + (rotVal * Math.sin(module0Angle)); // add the y components of the translation and rotation
+        double m0Speed = Math.sqrt( (Math.pow(m0x, 2)) + (Math.pow(m0y, 2))); // find the magnitude of the hypotenuse between the x and y components
+        double m0Angle = Math.atan2(m0y, m0x); // find the angle that the x and y vals make (IMPORTANT! This range is -pi to pi, could break something by not being 0 to 2pi)
+
+        // top left wheel 1
+        double module1Angle = (5 * Math.PI) / 4; // angle from the center of the robot to the module
+        double m1x = xVal + (rotVal * Math.cos(module1Angle)); // add the x components of the translation and rotation
+        double m1y = yVal + (rotVal * Math.sin(module1Angle)); // add the y components of the translation and rotation
+        double m1Speed = Math.sqrt( (Math.pow(m1x, 2)) + (Math.pow(m1y, 2))); // find the magnitude of the hypotenuse between the x and y components
+        double m1Angle = Math.atan2(m1y, m1x); // find the angle that the x and y vals make (IMPORTANT! This range is -pi to pi, could break something by not being 0 to 2pi)
+
+        // bottom left wheel 2
+        double module2Angle = (7 * Math.PI) / 4; // angle from the center of the robot to the module
+        double m2x = xVal + (rotVal * Math.cos(module2Angle)); // add the x components of the translation and rotation
+        double m2y = yVal + (rotVal * Math.sin(module2Angle)); // add the y components of the translation and rotation
+        double m2Speed = Math.sqrt( (Math.pow(m2x, 2)) + (Math.pow(m2y, 2))); // find the magnitude of the hypotenuse between the x and y components
+        double m2Angle = Math.atan2(m2y, m2x); // find the angle that the x and y vals make (IMPORTANT! This range is -pi to pi, could break something by not being 0 to 2pi)
+
+        // bottom right wheel 3
+        double module3Angle = (1 * Math.PI) / 4; // angle from the center of the robot to the module
+        double m3x = xVal + (rotVal * Math.cos(module3Angle)); // add the x components of the translation and rotation
+        double m3y = yVal + (rotVal * Math.sin(module3Angle)); // add the y components of the translation and rotation
+        double m3Speed = Math.sqrt( (Math.pow(m3x, 2)) + (Math.pow(m3y, 2))); // find the magnitude of the hypotenuse between the x and y components
+        double m3Angle = Math.atan2(m3y, m3x); // find the angle that the x and y vals make (IMPORTANT! This range is -pi to pi, could break something by not being 0 to 2pi)
+
+        /**
+         * Normalizes wheel speeds so no wheel is going over 100 percent
+         */
+
+         double maxWheelSpeed = m0Speed;
+         maxWheelSpeed = Math.max(maxWheelSpeed, m1Speed);
+         maxWheelSpeed = Math.max(maxWheelSpeed, m2Speed);
+         maxWheelSpeed = Math.max(maxWheelSpeed, m3Speed);
+         if (maxWheelSpeed > 1) {
+             m0Speed /= maxWheelSpeed;
+             m1Speed /= maxWheelSpeed;
+             m2Speed /= maxWheelSpeed;
+             m3Speed /= maxWheelSpeed;
+         }
+         
+         setSwerveUnitState(Motors.driveMotor3, Motors.turnMotor3, m_turningEncoderBottomRight, m3Speed, m3Angle, 0.2, 0.05);
+
+    }
+
+    public void setSwerveUnitState(CANSparkMax driveMotor, CANSparkMax rotateMotor, Encoder swerveUnitEncoder, double driveMotorSpeed, double moduleAngle, double driveMotorSpeedScale, double rotateMotorSpeedScale) {
+        driveMotor.set(driveMotorSpeed * driveMotorSpeedScale); //sets the drive motor power
+        
+        /**
+         * The following section is the rotation direction optimizer, and finds the shortest path to rotate the motor
+         */
+        double encoderRotation = ( (swerveUnitEncoder.getRaw() / 4096.0) % 1) * ( 2 * Math.PI); //convert the encoder val to radians. the mod 1 is because the rotation can go above and below a full rotation, so mod 1 removes extras
+        double rawDistanceFromSetpoint = encoderRotation - moduleAngle; // distance between the two points on the circle. can be + or -
+        double limitedDistance = rawDistanceFromSetpoint % (2 * Math.PI); // make sure value is within a full circle
+        boolean rotationDirection; //this bool is set to determine which direction the motor needs to rotate (cw true, ccw false)
+
+        if (limitedDistance < Math.PI) {
+            rotationDirection = limitedDistance > 0 ? true : false;
+        }
+        else {
+            rotationDirection = limitedDistance > 0 ? false : true;
+        }
+
+        if (Math.abs(limitedDistance) > (Math.PI / 32)){
+            rotateMotor.set(rotationDirection ? -rotateMotorSpeedScale : rotateMotorSpeedScale);
+        }
+        else {
+            rotateMotor.set(0);
+        }
+    }
+
 }
 
 
--- swerveBasic/src/main/java/frc/robot/Motors.java
+++ swerveBasic/src/main/java/frc/robot/Motors.java
@@ -7,8 +7,15 @@ import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 
 public class Motors {
-    public static CANSparkMax driveMotorBottomRight     = new CANSparkMax(1,  CANSparkMaxLowLevel.MotorType.kBrushless);
-    public static CANSparkMax turnMotorBottomRight     = new CANSparkMax(2,  CANSparkMaxLowLevel.MotorType.kBrushless);
-    public static CANSparkMax driveMotorTopRight = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
-    public static CANSparkMax turnMotorTopRight = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+    public static CANSparkMax driveMotor0    = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+    public static CANSparkMax turnMotor0     = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    public static CANSparkMax driveMotor1    = new CANSparkMax(3,  CANSparkMaxLowLevel.MotorType.kBrushless);
+    public static CANSparkMax turnMotor1     = new CANSparkMax(4,  CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    public static CANSparkMax driveMotor2    = new CANSparkMax(5,  CANSparkMaxLowLevel.MotorType.kBrushless);
+    public static CANSparkMax turnMotor2     = new CANSparkMax(6,  CANSparkMaxLowLevel.MotorType.kBrushless);
+
+    public static CANSparkMax driveMotor3    = new CANSparkMax(7,  CANSparkMaxLowLevel.MotorType.kBrushless);
+    public static CANSparkMax turnMotor3     = new CANSparkMax(8,  CANSparkMaxLowLevel.MotorType.kBrushless);
 }
